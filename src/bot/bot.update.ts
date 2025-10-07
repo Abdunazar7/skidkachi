33dbb817e4ddb11bbe920b5fc32b39b0b06b481c
Abdunazar <abdunazaribragimov945@gmail.com>
2025-10-07 17:42:12 +0500

    Bot Updated

--- src/bot/bot.update.ts
+++ src/bot/bot.update.ts
@@ -1,4 +1,4 @@
-import { Ctx, Start, Update } from "nestjs-telegraf";
+import { Ctx, On, Start, Update } from "nestjs-telegraf";
 import { BotService } from "./bot.service"
 import { Context } from "telegraf";
 
@@ -10,5 +10,19 @@ export class Botupdate {
   async start(@Ctx() ctx:Context){
     ctx.reply("Yangi Yiling Bilan Megajin");
   }
+
+  @On("text")
+  async OnText(@Ctx() ctx:Context){
+    // console.log(ctx)
+    if("text" in ctx.message!){
+      if(ctx.message.text=="hello"){
+        await ctx.replyWithHTML("<b>Salom</b>");
+      } else {
+        await ctx.replyWithHTML(ctx.message.text)
+      }
+    }
+  }
+
+
 
 }
